f36c0c96c904d28067ecc40d49fc203f4cbe2eeb
langletamelie <amelie.langlet@hotmail.com>
2018-06-25 10:04:51 +0200

    exo5

---
+++ assets/js/js5.js
@@ -0,0 +1,20 @@
+//déclaration des fonctions
+function changeImage(){
+document.getElementById('image1').src = 'assets/images/image1_2.jpg';
+}
+function changeImage2(){
+document.getElementById('image2').src = 'assets/images/image2_2.jpg';
+}
+function changeImage3(){
+document.getElementById('image3').src = 'assets/images/image3_2.jpg';
+  }
+function changeImage4(){
+document.getElementById('image4').src = 'assets/images/image4_2.jpg';
+}
+function changeImage5(){
+document.getElementById('image5').src = 'assets/images/image5_2.jpg';
+}
+// test déclaration de fonction unique
+function changeImage(){
+
+}
